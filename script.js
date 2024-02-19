e63634a7907bf21a3df8c8330d7f3ef9c121025b
FonKnp <fonnfonk@gmail.com>
2024-02-19 16:56:38 -0500

    add code in JavaScript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var generateBtn = document.querySelector("#generate");
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var numbers = '0123456789';
+var numberS = '0123456789';
 var specialCharacters = '~`! @#$%^&*()-_+={}[]|\\;:"<>,./?';
 
 
@@ -19,29 +19,45 @@ function writePassword() {
 
 
 // Function to generate combination of password 
-function generatePass() {
-  let pass = '';
+function generatePassword() {
   
-  let passLength = prompt('How many characters in the password?');
-  for (passLength > 128 || passLength < 8) {
-    alert('Password must contain 8-128 characters.')
-    return passLength;
-  }
- 
+  let pass = '';
 
-  for (let i = 1; i <= 8; i++) {
-      let char = Math.floor(Math.random()
-          * str.length + 1);
+  //define how long the password will be
+  let passLength = parseInt(window.prompt('How many characters in the password?'));
 
-      pass += str.charAt(char)
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert('Password must contain 8-128 characters.');
+    return;
   }
 
-  return pass;
+  if (window.confirm('Would you like an uppercase in the password?')) {
+    pass += upperCase;
+  }
+
+  if (window.confirm('Would you like a lowercase in the password?')) {
+    pass += lowerCase;
+  }
+
+  if (window.confirm('Would you like numbers in the password?')) {
+    pass += numberS;
+  }
+
+  if (window.confirm('Would you like special characters in the password?')) {
+    pass += specialCharacters;
+  }
+
+  if (pass === '') {
+    alert('At least one character type must be selected.');
+    return;
+  }
+
+  let passcode = '';
+  for (let i = 0; i < passLength; i++) {
+    let randomIndex = Math.floor(Math.random() * pass.length);
+    passcode += pass[randomIndex];
+  }
+  return passcode;
 }
-
-console.log(generatePass());
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-console.log(numbers);
